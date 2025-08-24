50ac525e904bb177a7f1d0f349b5f17f2bbaa4c6
KyungTae Kim <kimkyungtae12386@gmail.com>
2025-08-23 23:36:44 -0700

    vercel deploy3

--- app/submissionPage/page.tsx
+++ app/submissionPage/page.tsx
@@ -26,7 +26,7 @@ const genres = [
 ];
 
 const companies = [
-    "88rising", "H1ghr Music", "88rsigng", "SM Entertainment", "Other"
+    "88rising", "H1ghr Music", "SM Entertainment", "Other"
 ];
 
 export default function DemoSubmissionForm() {
